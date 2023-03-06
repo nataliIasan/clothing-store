b7d8d601abc3261d07b995d0ebe150ce96ccfb17
nataliIasan <nataliaiasan.dev@gmail.com>
2023-03-06 14:32:13 +0100

    first commit

---
+++ src/Clothes.js
@@ -0,0 +1,20 @@
+function Clothes({itemForSale}) {
+    return(
+        <div className="products">
+            {itemForSale.map((element => {
+                const {id, name, price, image} = element;
+                return(
+                    <div className="product-card" key={id}>
+                        <img src={image} width="350px" height="470px"/>
+                    <div className="product-info">
+                        <h3>{name}</h3>
+                        <h4>{price}</h4>
+                    </div>
+                    </div>
+                )
+            }))}
+        </div>
+    )
+}
+
+export default Clothes;
